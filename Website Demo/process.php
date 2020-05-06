9ee466d188c30312a0e15917ee4a7e96ce17e3e8
Yash Vardhan <59708439+VardhanYash@users.noreply.github.com>
2020-05-05 22:34:27 -0400

    Add files via upload

---
+++ Website Demo/process.php
@@ -0,0 +1,18 @@
+<?php ob_start();
+    if(isset($_POST['submit'])) {
+
+
+        $to = "adiyash2000@gmail.com";
+        $subject = $_POST['subject'];
+        $email = $_POST['email'];
+        $txt = $_POST['message'];
+        $headers = "From: " .$email . "\r\n" .
+        "CC: somebodyelse@example.com";
+        
+        mail($to, $subject, $message, $headers);
+
+        header("Location: contact.html");
+    }
+
+
+?>
